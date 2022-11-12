8d3603d65f2e10b1b7b1842ea56d84038a2d2ddf
a.sitnikov <timonsitnik@gmail.com>
2022-11-12 21:02:39 +0700

    OOP

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<User> userListOriginal = new ArrayList<>();
+        ArrayList<User> userListIsFile = new ArrayList<>();
+
+        FileWriter nFile = new FileWriter("userList.txt");
+
+
+        for (int i = 0; i <= 10; i++) {
+            userListOriginal.add(new Individual("individual", "NSK", "89831344087", "Manager"));
+        }
+
+        userListOriginal.forEach(user -> {
+            try {
+                nFile.write(user.getName() + " ");
+                nFile.write(user.getAddress() + " ");
+                nFile.write(user.getPhoneNumber() + "\n");
+                nFile.write(user. instanceof Individual.getPost() + "\n");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        nFile.close();
+
+        FileReader fr = new FileReader("userList.txt");
+        Scanner scan = new Scanner(fr);
+
+        while (scan.hasNextLine()) {
+
+            String[] lineSplit = scan.nextLine().split(" ");
+            userListIsFile.add(new Individual(lineSplit[0], lineSplit[1], lineSplit[2]));
+
+        }
+        fr.close();
+
+        userListOriginal.forEach(System.out::println);
+        System.out.println();
+        userListIsFile.forEach(System.out::println);
+
+        for (int i = 0; i < userListIsFile.size(); i++) {
+            System.out.println(userListOriginal.get(i));
+            System.out.println(ENV.compare(userListOriginal.get(i), userListIsFile.get(i)));
+        }
+
+    }
+
+}
---
+++ User.java
@@ -0,0 +1,132 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class ENV {
+    static int ID = 0;
+
+    static boolean compare(User a, User b) {
+
+        return a.getId() == b.getId() &
+                a.getName().equals(b.getName()) &
+                a.getAddress().equals(b.getAddress()) &
+                a.getPhoneNumber().equals(b.getPhoneNumber());
+    }
+
+}
+
+abstract class User {
+
+    //private static ArrayList<User> userListOriginal = new ArrayList<>();
+    //private static ArrayList<User> userListIsFile = new ArrayList<>();
+
+    private int id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+//    private int age;
+//    private String gender;
+
+    public User() {
+        id = ENV.ID;
+        ENV.ID++;
+
+        name = "";
+        address = "";
+        phoneNumber = "";
+
+    }
+
+    public User(String name, String address, String phoneNumber) {
+        id = ENV.ID;
+        ENV.ID++;
+
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User(int id, String name, String address, String phoneNumber) {
+        this.id = id;
+        ENV.ID++;
+
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+//    public static void wtiteToFile() {
+//        try {
+//            FileWriter nFile = new FileWriter("userList.txt");
+//
+//            userListOriginal.forEach(user -> {
+//                nFile.write(user.getName() + " ");
+//                nFile.write(user.getAddress() + " ");
+//                nFile.write(user.getPhoneNumber() + "\n");
+//            });
+//            nFile.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+//    public static ArrayList<User> readToFile() {
+//        FileReader fr = new FileReader("userList.txt");
+//        Scanner scan = new Scanner(fr);
+//
+//        while (scan.hasNextLine()) {
+//
+//            String[] lineSplit = scan.nextLine().split(" ");
+//            userListIsFile.add(new Individual(lineSplit[0], lineSplit[1], lineSplit[2]));
+//
+//        }
+//        fr.close();
+//
+//        return userListIsFile;
+//    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", ";
+    }
+}
